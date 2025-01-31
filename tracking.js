5a2c070d7cc89413e427ce220072e8c4c679913d
Alex Phelps <alexphelps3@gmail.com>
2025-01-31 11:48:08 +0700

    refeinement

--- tracking.js
+++ tracking.js
@@ -1,9 +1,13 @@
 if (app.settings.google_tag_manager_enabled) {
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(conifg) { dataLayer.push(arguments); }
     (function (w, d, s, l, i) {
         w[l] = w[l] || []; w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'
         }); var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
         'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
     })(window, document, 'script', 'dataLayer', app.settings.google_tag_manager_container_id)
+
 
     let prepareLineItems = (event) => {
         let result = []
@@ -20,9 +24,18 @@ if (app.settings.google_tag_manager_enabled) {
         return result;
     }
 
+    analytics.subscribe('page_viewed', (event) => {
+        window.dataLayer.push({
+            event: "page_view",
+            page_path: window.parent.location.pathname,
+            page_location: window.parent.location.href,
+            page_title: window.parent.document.title
+        });
+    });
+
     analytics.subscribe('product_viewed', (event) => {
-        dataLayer.push({ ecommerce: null });
-        dataLayer.push({
+        window.dataLayer.push({ ecommerce: null });
+        window.dataLayer.push({
             event: 'view_item',
             ecommerce: {
                 currency: event.data?.purchase_info?.price?.currency,
@@ -43,8 +56,8 @@ if (app.settings.google_tag_manager_enabled) {
     });
 
     analytics.subscribe('product_added_to_cart', (event) => {
-        dataLayer.push({ ecommerce: null });
-        dataLayer.push({
+        window.dataLayer.push({ ecommerce: null });
+        window.dataLayer.push({
             event: 'add_to_cart',
             ecommerce: {
                 currency: event.data?.purchase_info?.price?.currency,
@@ -64,8 +77,8 @@ if (app.settings.google_tag_manager_enabled) {
     });
 
     analytics.subscribe('checkout_started', (event) => {
-        dataLayer.push({ ecommerce: null });
-        dataLayer.push({
+        window.dataLayer.push({ ecommerce: null });
+        window.dataLayer.push({
             event: 'begin_checkout',
             ecommerce: {
                 currency: event.data?.currency,
@@ -77,8 +90,8 @@ if (app.settings.google_tag_manager_enabled) {
     });
 
     analytics.subscribe('checkout_completed', (event) => {
-        dataLayer.push({ ecommerce: null });
-        dataLayer.push({
+        window.dataLayer.push({ ecommerce: null });
+        window.dataLayer.push({
             event: 'purchase',
             ecommerce: {
                 currency: event.data?.currency,
